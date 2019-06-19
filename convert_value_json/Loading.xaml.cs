40411c631e37bb928881b79eaebaf25a21226022
Mark S <mr.mark.ru@gmail.com>
2019-06-19 20:51:51 +0700

    Удалил ожидание

--- convert_value_json/Loading.xaml.cs
+++ convert_value_json/Loading.xaml.cs
@@ -38,7 +38,6 @@ namespace convert_value_json
 
         void Run()
         {
-            Task.Delay(2000).Wait();
             WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
             var json = wc.DownloadString("https://www.cbr-xml-daily.ru/daily_json.js");
             var user = JsonConvert.DeserializeObject<RootObject>(json);
